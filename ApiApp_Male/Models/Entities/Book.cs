64885797c58b9718e5582048350743671e0820e5
bader <bedoo7860@hotmail.com>
2020-03-25 00:04:07 +0200

    Inital Version Using Database

---
+++ ApiApp_Male/Models/Entities/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiApp_Male.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        [Required]
+        public String BookName { get; set; }
+        public int PublishYear { get; set; }
+        public int PaperCount { get; set; }
+
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
---
+++ ApiApp_Male/Models/LibraryDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiApp_Male.Models
+{
+    public class LibraryDBContext : DbContext
+    {
+        public LibraryDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
